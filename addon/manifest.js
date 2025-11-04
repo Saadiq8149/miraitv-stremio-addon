7f260a0cf4a9a4eb7506656e8587ad6fe770863b
Saadiq <101512821+Saadiq8149@users.noreply.github.com>
2025-11-04 22:25:17 +0530

    fix: update logo URL in manifest to point to the correct GitHub asset

--- addon/manifest.js
+++ addon/manifest.js
@@ -4,7 +4,7 @@ export const manifest = {
   name: "MiraiTV",
   description:
     "Fast streaming with multiple sources (non-torrent) for movies and TV shows",
-  logo: "https://miraitv-stremio.ddnsfree.com/assets/logo.png",
+  logo: "https://github.com/Saadiq8149/miraitv-stremio-addon/blob/87fb4dbcc5d6ba38a000d85d8dcc59963b8245ee/assets/logo.png",
   types: ["movie", "series"],
   resources: ["stream", "subtitles"],
   catalogs: [],
